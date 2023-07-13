7eb9315886de8f1d71f3035e0d62f957ddfa909d
stanley07 <ezebo001@gmail.com>
2023-07-13 05:45:21 +0100

    database

--- building/forms.py
+++ building/forms.py
@@ -3,7 +3,7 @@ from .models import Building
 
 class BuildingForm(forms.ModelForm):
     owner_name = forms.CharField(label="Owner's Name")
-    geographical_zone = forms.CharField(label="Geographical Zone")
+    L_G_A = forms.CharField(label="L.G.A")
     number_flats = forms.CharField(label="Number Of Flats")
     email = forms.EmailField(label="E-mail")
     phone_number = forms.CharField(label="Phone Number")
@@ -11,4 +11,4 @@ class BuildingForm(forms.ModelForm):
 
     class Meta:
         model = Building
-        fields = ('owner_name', 'address', 'building_type', 'number_flats', 'amount_value',  'ansidd_no', 'geographical_zone', 'email', 'phone_number', 'agent_id', 'image')
+        fields = ('owner_name', 'address', 'building_type', 'number_flats', 'amount_value',  'ansidd_no', 'L_G_A', 'email', 'phone_number', 'agent_id', 'image')
